cf172190754403195c2f0f0892257c90ba09d065
palkeo <palkeo@gmail.com>
2013-03-21 22:46:56 +0100

    Correction du reset

--- code/src/reset.c
+++ code/src/reset.c
@@ -6,19 +6,18 @@
 #include "globals.h"
 #include "extern_globals.h"
 
-int reset(void) {    
-	
-	interruptionCamera(true);  // on fait la balance des blancs
-        //SIU.PGPD0[2].R & = 0xf0f
+void reset(void)
+{    
+    interruptionCamera(true);  // on fait la balance des blancs
+    //SIU.PGPD0[2].R & = 0xf0f
 
-        do
-        {
+    do
+    {
             
-s
-        }
-        while(SIU.PGPDI[2].R & 0x10000000);
+    }
+    while(SIU.PGPDI[2].R & 0x10000000);
         
-        SIU.PGPDO[2].R &= 0xf0ffffff; // Enable all leds
-        delay(10000000);
-        SIU.PGPDO[2].R |= 0x0f000000; // Disable all leds  	  
+    SIU.PGPDO[2].R &= 0xf0ffffff; // Enable all leds
+    delay(10000000);
+    SIU.PGPDO[2].R |= 0x0f000000; // Disable all leds  	  
 }
