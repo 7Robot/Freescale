28780bb876157c877d037e32c2ba1f2c60379c44
7Robot <robot@list.bde.enseeiht.fr>
2013-03-25 16:08:19 +0100

    Nouveau code arret : a tester

--- code/src/codeArretC.c
+++ code/src/codeArretC.c
@@ -1,52 +1,59 @@
 #include "codeArretC.h"
 #include "extern_globals.h"
+#define min(i, j) (i < j ? i : j) 
+#define max(i, j) (i > j ? i : j)
+#define abs(i) (i < 0 ? -1 : i)
+
+void recherche_pic (int8_t valeurs_derivee[2][126], uint8_t* pos_max) {
+	uint8_t i, k, sup, inf ;
+		// Recherche de la premiere valeur de la derivee non encore assimilee a un pic
+		k = 0;		
+		while (! valeurs_derivee[1][k]) k++;				
+		*pos_max = k;
+		
+		// Recherche du j-ieme max
+		for (i=k+1; i<126; i++) {
+			if (valeurs_derivee[1][i] && abs(valeurs_derivee[0][i]) > abs(valeurs_derivee[0][*pos_max])) 
+			    *pos_max = i;
+		}
+		
+		// Consideration du -ieme max et de ses points voisins
+		inf = max(*pos_max -2, 0);
+		sup = min(*pos_max +2, 126);
+		for (i = inf; i < sup; i++)  valeurs_derivee[1][i] = 0;
+}	 
 
 int code_arret_cam(void)
 {
-
-	static uint8_t nbZero = 0;
-	static uint8_t nbLigneArrivee = 0;
+	static uint8_t nb_pic = 5;
+	static uint8_t seuil = 50;
 	
-	int8_t valeursDerivee[126];		
-	uint8_t i; 
-	uint8_t posMax = 0;
-	uint8_t moyenne = 0;
+	int8_t valeurs_derivee[2][126];
+	uint8_t j; 
+	uint8_t pos_max_prec, pos_max;
 	
-	// Calcul de la dérivée, du maximum et de la moyenne
-    for (i=0; i<126; i++) {
-		 valeursDerivee[i] = camera_valeurs[i+2] - camera_valeurs[i];
-		 posMax = (abs(valeursDerivee[i]) > abs(valeursDerivee[posMax]) ? i : posMax);
-		 moyenne += camera_valeurs[i];		 
+    // Initialisation de la dérivée
+    for (j=0; j<126; j++) {
+		 valeurs_derivee[0][j] = camera_valeurs[j+2] - camera_valeurs[j];
+		 valeurs_derivee[1][j] = 1; 
     }
     
-    moyenne /= 127;
+    // Recherche des 8 pics de la derivee (= caracteristique de la ligne d'arrivee)
+    for (j=0; j<nb_pic; j++) {
+	    recherche_pic(valeurs_derivee, &pos_max);	
+	}
+	    
+	j = 0;
+	do 
+	{
+	    j++;
+	    pos_max_prec = pos_max;
+	    recherche_pic(valeurs_derivee, &pos_max);	    
+	} 
+	while (j< (8 -nb_pic +1) && (valeurs_derivee[0][pos_max_prec] - valeurs_derivee[1][pos_max]) < seuil); 
     
-    // Seuil de considération (algorithme) = k1 * moyenne => incertitude
-//    if (camera_valeurs[posMax] > 3/2 * moyenne) { 		
-       // Detection des pics de la derivee
-       for (i=0; i<126; i++) {
-		  
-		  // Seuil de considération (pic) = k2 * max => incertitude
-		  if (abs(valeursDerivee[i]) > (6/10)*abs(valeursDerivee[posMax])) {
-			  nbZero++;	
-              i += 5;	
-		  }	  
-//       }   
-    
-       // Detection des lignes d'arrivees intermediaires (au nombre de deux)
-       if (nbZero >= 6) {
-	   // nbLigneArrivee++;
-           nbZero = 0;
-           return 1; // provisoire   
-       }
-       else return 0;
-       /*
-       // Detection de LA ligne d'arrivee
-       if (nbLigneArrivee >= 3) {
-	   nbLigneArrivee = 0;
-           return 1;		   	   
-       }
-       */
-       
-    } 	
+    if ((valeurs_derivee[0][pos_max_prec] - valeurs_derivee[1][pos_max]) < seuil)
+        return 1;
+    else
+        return 0;   
 }
